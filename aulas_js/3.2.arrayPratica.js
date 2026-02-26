0a0461bf94558e6fbc62c7d68f33bc28db0aa70e
Paulo Ferreira <paulo2ferreira@gmail.com>
2026-02-26 09:01:17 -0300

    Add pratica de array

--- aulas_js/3.2.arrayPratica.js
+++ aulas_js/3.2.arrayPratica.js
@@ -1,11 +1,30 @@
 let alunos = ['João', 'Maria',
     'Ana', 'José'];
+console.log(alunos);
 
 // Implementar a remoção dado]
 // uma posição
 let indiceRemover = 2;
 // fazer implementação usando slice
+const arrayPrimeiraParte = alunos.slice(0, indiceRemover);
+const arraySegundaParte =
+    alunos.slice(indiceRemover + 1, alunos.length);
+// concat
+const resultadoComConcat =
+    arrayPrimeiraParte.concat(arraySegundaParte);
+console.log(resultadoComConcat);
+// spread
+const resultadoComSpread =
+    [...arrayPrimeiraParte, ...arraySegundaParte];
+console.log(resultadoComSpread);
+// + (saber o que dá)
+const resultadoComAdicao =
+    arrayPrimeiraParte + arraySegundaParte;
+console.log(resultadoComAdicao);
+
 
 // Implementar a remoção dado o conteúdo
 // Usar indexOf
-let nomeRemover = 'João'
+let nomeRemover = 'José';
+
+indiceRemover = alunos.indexOf(nomeRemover);
